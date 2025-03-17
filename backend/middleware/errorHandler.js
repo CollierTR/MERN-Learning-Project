689a85b9e9c81f37a849da19f0a3cfed35bec4ab
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-03-16 20:38:30 -0500

    first commit

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,20 @@
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const { logEvents } = require('./logger')
+
+const errorHandler = (err, req, res, next) => {
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+    console.log(err.stack)
+
+    const status = res.statusCode ? res.statusCode : 500
+    res.status(status)
+
+    res.json({ message: err.message })
+    next()
+}
+
+module.exports = errorHandler
